0340f4061628b8f31a3ed380b35f4a34f8bf1896
Arthur Lara <avlara@hotmail.com>
2017-07-19 13:50:55 -0300

    Automatização de compilação da GUI e criação de link simbólico

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd ~
 
 export PROVANT_ROS=${HOME}/catkin_ws/src
 export TILT_STRATEGIES=${HOME}/catkin_ws/devel/lib/
-export TILT_PROJECT=${PROVANT_ROS}/provant_simulator
+export TILT_PROJECT=${PROVANT_ROS}/ProVANT-Simulator
 export TILT_CONFIG=${TILT_PROJECT}/source/Database/simulation_elements/models/real/vant_2comcargaMarcelo/config/config.xml
 export TILT_MATLAB=${TILT_PROJECT}/source/Structure/Matlab/
 export PROVANT_DATABASE=${TILT_PROJECT}/source/Database
@@ -13,4 +13,10 @@ export DIR_ROS=${HOME}/catkin_ws
 cd catkin_ws
 catkin_make --pkg simulator_msgs
 catkin_make
+
+mkdir -p ${TILT_PROJECT}/source/build
+cd ${TILT_PROJECT}/source/build
+qmake ${TILT_PROJECT}/source/GUI/GUI.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug
+make 
+sudo ln -s ${TILT_PROJECT}/source/build/GUI /usr/local/bin/provant_gui
 
--- source/GUI/startup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import os
-os.system("source /opt/ros/kinetic/setup.bash")
-os.system("source /home/macro/catkin_ws/devel/setup.bash")
-os.system("roslaunch empty gazebo.launch")
-
