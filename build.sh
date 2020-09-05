608785764a85b4c6cf2cfc817ed5b795d994005a
Júnio Eduardo de Morais Aquino <jeduardo@ufmg.br>
2020-09-05 06:57:42 -0300

    Remove explicit compilation of the simulator_msgs package
    This step was only needed to prevent errors arrising from the undeclared dependencies on the simulator_msgs package, as this was fixed, it is redundant.
    
    Furthermore, this step masked the undeclared dependencies issue when building using the install and build scripts.

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ cd ~
 
 # Entrando no espaço de trabalho do ROS
 cd catkin_ws
-# Compilando estruturas de mensagem utilizadas na simulação utilizando controlador implementado em software local 
-catkin_make --pkg simulator_msgs
 #Compilando demais pacotes existentes no ambiente de trabalho do ROS
 catkin_make
 
--- install.sh
+++ install.sh
@@ -20,8 +20,6 @@ echo "export DIR_ROS=${HOME}/catkin_ws" >> ${HOME}/.bashrc
 
 # Entrando no espaço de trabalho do ROS
 cd catkin_ws
-# Compilando estruturas de mensagem utilizadas na simulação utilizando controlador implementado em software local 
-catkin_make --pkg simulator_msgs
 #Compilando demais pacotes existentes no ambiente de trabalho do ROS
 catkin_make
 
