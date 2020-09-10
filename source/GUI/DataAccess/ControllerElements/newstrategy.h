3949566d91b620a062be878a9678f747604ec8b9
Júnio Eduardo de Morais Aquino <jeduardo@ufmg.br>
2020-09-10 11:34:26 -0300

    Update generation of the new controllers
    The new controller generation now creates newer versions of the CMakeLists.txt and of the new formato for the package.xml file.
    
    Also, the main.cpp was updated to follow a cleaner coding style.

--- source/GUI/DataAccess/ControllerElements/newstrategy.h
+++ source/GUI/DataAccess/ControllerElements/newstrategy.h
@@ -16,6 +16,22 @@ class NewStrategy
 public:
     NewStrategy();
     static void createProject(const QString &name);
+
 };
+
+/**
+ * @brief capitalizeClassName Returns a camel cased version of a name.
+ *
+ * For example, the following inputs should return
+ * test_quad -> TestQuad
+ * TEST_QUAD -> TestQuad
+ * TESTQUAD -> Testquad
+ * testquad -> Testquad
+ * TestQuad -> TestQuad
+ *
+ * @param name class name.
+ * @return CamelCased class name.
+ */
+QString capitalizeClassName(const QString &name);
 
 #endif // NEWSTRATEGY_H
